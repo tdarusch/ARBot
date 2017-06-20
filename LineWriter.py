30ea9161d95f97c75ae9e89f8d6850826d505abe
Tom <tommy_rusch@aol.com>
2017-06-19 23:07:04 -0400

    Add files via upload

---
+++ LineWriter.py
@@ -0,0 +1,5 @@
+def newline(a,b,c,d,e,f,g,h,i):
+    print("'" + str(a) + "': [" + '"'
+          + str(a).upper() + '"' + ", " + str(b) + ", " + str(c) + ", " + str(d) + ", " + str(e) + ", "
+          + str(f) + ", " + str(g) + ", " + '"' + str(h) + '", "' + str(i) + '"],')
+
